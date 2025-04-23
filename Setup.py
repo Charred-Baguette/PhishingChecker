43666afc706b70bc73fa079ab86d6e41a0828791
Charred-Baguette <118027930+Charred-Baguette@users.noreply.github.com>
2025-04-23 11:14:35 -0600

    Create Setup.py

---
+++ Setup.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import os
+import kagglehub
+
+def dataset():
+    path = kagglehub.dataset_download("ethancratchley/email-phishing-dataset")
+
+    print("Path to dataset files:", path)
+    df = pd.read_csv(os.path.join(path, "email_phishing_data.csv"))
+    return df
+if __name__ == "__main__":
+    df = dataset()
+    df.head()
+    
